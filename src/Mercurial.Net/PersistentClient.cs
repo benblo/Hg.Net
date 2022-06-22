77bbda48b4a0d5ede07e27bb0163d0e5e62c5b70
Benoit Fouletier <benoit.fouletier@gmail.com>
2022-06-22 11:54:39 +0100

    fix for .NET Core: `System.Runtime.Remoting` is not available anymore see https://docs.microsoft.com/en-us/dotnet/core/porting/net-framework-tech-unavailable

--- src/Mercurial.Net/PersistentClient.cs
+++ src/Mercurial.Net/PersistentClient.cs
@@ -5,11 +5,22 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Runtime.Remoting;
+//using System.Runtime.Remoting;
 using System.Text;
 
 namespace Mercurial
 {
+    /// <summary>The exception that is thrown to communicate errors to the client when the client connects to non-.NET Framework applications that cannot throw exceptions.</summary>
+    public class ServerException : SystemException
+    {
+        /// <summary>Initializes a new instance of the <see cref="T:System.Runtime.Remoting.ServerException" /> class with a specified error message and a reference to the inner exception that is the cause of this exception.</summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <param name="innerException">The exception that is the cause of the current exception. If the <paramref name="innerException" /> parameter is not <see langword="null" />, the current exception is raised in a <see langword="catch" /> block that handles the inner exception.</param>
+        public ServerException(string message, Exception innerException = null) : base(message, innerException)
+        {
+        }
+    }
+
     /// <summary>
     /// This class implements <see cref="IClient"/> by spinning up an instance when
     /// first instantiated, and keeping the instance around and communicating with it
